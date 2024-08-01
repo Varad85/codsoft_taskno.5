7f15c656ab226ef0324467f684996e5875dfe84b
Varad Kanekar <kanekarvarad@gmail.com>
2024-08-01 19:57:24 +0530

    method to add course to database

---
+++ CourseDatabase.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Class to manage the course database
+public class CourseDatabase {
+    private List<Course> courses;
+
+    // Constructor to initialize the course database
+    public CourseDatabase() {
+        this.courses = new ArrayList<>();
+    }
+
+    // Method to add a course to the database
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+}
